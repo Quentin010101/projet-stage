fe575cf6ba00dca518ed7ebfbacc5bb8eccae6eb
Quentin <q_cozic@hotmail.fr>
2022-03-08 16:10:41 +0100

    j

--- View/template/template.php
+++ View/template/template.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pageTitle; ?></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../css/general.css" type="text/css">
-    <link rel="stylesheet" href="../css/header-footer.css" type="text/css">
+    <link rel="stylesheet" href="/css/general.css" type="text/css">
+    <link rel="stylesheet" href="/css/header-footer.css" type="text/css">
     
     
     <link href='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css' rel='stylesheet' />
@@ -16,7 +16,7 @@
 
     <?php if (isset($pageCss)) :
     ?>
-        <link rel="stylesheet" type="text/css" href="<?php echo '../css/' . $pageCss; ?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo '/css/' . $pageCss; ?>">
     <?php
     endif;
     ?>
